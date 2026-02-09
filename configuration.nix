2738c40b7f16ed89e4530b38eaefea5470bb170c
Samarth <zalkikarsamarth898@gmail.com>
2026-02-09 17:57:05 +0530

    feat: swayidle

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,9 @@
-{ config, lib, pkgs, ... }:
-
-let
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
   custom-astronaut-theme = pkgs.stdenv.mkDerivation {
     name = "sddm-astronaut-theme";
     src = ./themes;
@@ -9,24 +12,19 @@ let
       cp -r . $out/share/sddm/themes/sddm-astronaut-theme
     '';
   };
-in
-
-let
+in let
   home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz;
-in
-
-{
-  imports =
-    [ 
-      ./hardware-configuration.nix
-      (import "${home-manager}/nixos")
-    ];
+in {
+  imports = [
+    ./hardware-configuration.nix
+    (import "${home-manager}/nixos")
+  ];
 
   # Home-manager
-   home-manager.useGlobalPkgs = true;
-   home-manager.useUserPackages = true;
-   home-manager.backupFileExtension = "backup";
-   home-manager.users.samarth = import ./home.nix;
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "backup";
+  home-manager.users.samarth = import ./home.nix;
 
   # unfree
   nixpkgs.config.allowUnfree = true;
@@ -64,188 +62,193 @@ in
   };
 
   # Keyboard
-   services.xserver.xkb = {
-      layout = "us";
-      variant = "";
-   };
-   console.useXkbConfig = true;
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+  console.useXkbConfig = true;
 
   # Enable sound.
-   services.pipewire = {
-     enable = true;
-     pulse.enable = true;
-   };
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
-   services.libinput.enable = true;
+  services.libinput.enable = true;
 
-   users.users.samarth = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" ];
-     shell = pkgs.zsh;
-     packages = with pkgs; [
-       tree
-     ];
-   };
+  users.users.samarth = {
+    isNormalUser = true;
+    extraGroups = ["wheel"];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+      tree
+    ];
+  };
 
-  # CUSTOM STUFF 
-   programs.firefox.enable = true;
-   programs.zsh.enable = true;
-   programs.dconf.enable = true;
- 
+  # CUSTOM STUFF
+  programs.firefox.enable = true;
+  programs.zsh.enable = true;
+  programs.dconf.enable = true;
+
   # Wayland stuff
-   environment.sessionVariables = {
-     ADW_DISABLE_PORTAL = "1"; 
-     NIXOS_OZONE_WL = "1";
-   };
+  environment.sessionVariables = {
+    ADW_DISABLE_PORTAL = "1";
+    NIXOS_OZONE_WL = "1";
+  };
 
-    xdg.portal = {
-      enable = true;
-      wlr.enable = true; 
-      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-      config.common.default = "*";
-    };
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-gtk];
+    config.common.default = "*";
+  };
 
   # Sway
-   programs.sway = {
-     enable = true;
-     wrapperFeatures.gtk = true;
-   };
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
 
   # List packages installed in system profile.
-   environment.systemPackages = with pkgs; [
-     vim
-     grim
-     slurp
-     xfce.thunar
-     xfce.thunar-volman
-     xfce.thunar-media-tags-plugin
-     xfce.thunar-archive-plugin
-     xfce.thunar-vcs-plugin
-     neovim
-     wget
-     qutebrowser
-     mpv
-     rofi
-     rofi-emoji
-     waybar
-     foot
-     kitty
-     zip
-     unzip 
-     swww
-     swayimg
-     zathura
-     vesktop
-     opencode
-     docker
-     git
-     wl-clipboard
-     swaylock
-     adwaita-icon-theme
-     swayidle
-     typst
-     tmux
-     papirus-icon-theme
-     gruvbox-dark-gtk
-     libreoffice-fresh
-     nwg-look
-     obsidian
-     lua
-     antigravity
-     vscode
-     htop 
-     btop 
-     fastfetch
-     imagemagick
-     xdg-user-dirs
-     fzf 
-     yazi
-     gtk3
-     gtk4
-     spotify
-     autotiling
-     cliphist
-     apple-cursor
-     custom-astronaut-theme
-     ollama
-     gcc
-     gnumake
-     tree-sitter
-     lazygit
-     playerctl
-     lm_sensors
-     ncmpcpp
-     mpd 
-     mpc
-     rmpc
-   ];
- 
+  environment.systemPackages = with pkgs; [
+    vim
+    grim
+    slurp
+    xfce.thunar
+    xfce.thunar-volman
+    xfce.thunar-media-tags-plugin
+    xfce.thunar-archive-plugin
+    xfce.thunar-vcs-plugin
+    neovim
+    wget
+    qutebrowser
+    mpv
+    rofi
+    rofi-emoji
+    waybar
+    foot
+    kitty
+    zip
+    unzip
+    swww
+    swayimg
+    zathura
+    vesktop
+    opencode
+    docker
+    git
+    wl-clipboard
+    swaylock
+    adwaita-icon-theme
+    swayidle
+    typst
+    tmux
+    papirus-icon-theme
+    gruvbox-dark-gtk
+    libreoffice-fresh
+    nwg-look
+    obsidian
+    lua
+    antigravity
+    vscode
+    htop
+    btop
+    fastfetch
+    imagemagick
+    xdg-user-dirs
+    fzf
+    yazi
+    gtk3
+    gtk4
+    spotify
+    autotiling
+    cliphist
+    apple-cursor
+    custom-astronaut-theme
+    ollama
+    gcc
+    gnumake
+    tree-sitter
+    lazygit
+    playerctl
+    lm_sensors
+    ncmpcpp
+    mpd
+    mpc
+    rmpc
+    papirus-folders
+  ];
+
   # Fonts
-   fonts.packages =  with pkgs; [
-     noto-fonts
-     noto-fonts-cjk-sans
-     noto-fonts-color-emoji
-     jetbrains-mono
-     nerd-fonts.jetbrains-mono
-     nerd-fonts.fira-code
-     nerd-fonts.caskaydia-cove
-   ];
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-color-emoji
+    jetbrains-mono
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.fira-code
+    nerd-fonts.caskaydia-cove
+  ];
 
   # SERVICES
 
-   security.pam.services.swaylock = {};
-   services.gnome.gnome-keyring.enable = true;
-   services.gvfs.enable = true;
-   services.udisks2.enable = true;
-   services.tumbler.enable = true;
-  
-   services.mpd = {
-     enable = true;
-     musicDirectory = "/home/samarth/Music";
-     user = "samarth";
-     extraConfig = ''
-       audio_output {
-         type "pipewire"
-         name "My PipeWire Output"
-       }
-     '';
-   };
+  security.pam.services.swaylock = {};
+  services.gnome.gnome-keyring.enable = true;
+  services.gvfs.enable = true;
+  services.udisks2.enable = true;
+  services.tumbler.enable = true;
+  services.logind.settings = {
+    Login = {
+      HandleLidSwitch = "suspend";
+      LidSwitchIgnoreInhibited = "no";
+    };
+  };
 
-systemd.services.mpd.serviceConfig = {
-  ProtectHome = "read-only"; # Allows MPD to see into /home/samarth
-  ProtectSystem = "full";
-  Environment = "XDG_RUNTIME_DIR=/run/user/1000"; 
-};
+  services.mpd = {
+    enable = true;
+    musicDirectory = "/home/samarth/Music";
+    user = "samarth";
+    extraConfig = ''
+      audio_output {
+        type "pipewire"
+        name "My PipeWire Output"
+      }
+    '';
+  };
 
+  systemd.services.mpd.serviceConfig = {
+    ProtectHome = "read-only"; # Allows MPD to see into /home/samarth
+    ProtectSystem = "full";
+    Environment = "XDG_RUNTIME_DIR=/run/user/1000";
+  };
 
-   services.ollama = {
-     enable = true;
-     # loadModels = [ "llama3.2:3b" "deepseek-r1:1.5b"];
-   };
- 
+  services.ollama = {
+    enable = true;
+    # loadModels = [ "llama3.2:3b" "deepseek-r1:1.5b"];
+  };
+
   # Display Manager
-   services.displayManager.sddm = {
-       enable = true;
-       package = pkgs.kdePackages.sddm;
-       theme = "sddm-astronaut-theme";
-       extraPackages = with pkgs; [
-         kdePackages.qtsvg
-         kdePackages.qtmultimedia
-         kdePackages.qtvirtualkeyboard
-         kdePackages.qt5compat
-       ];
-       wayland.enable = true;
-       settings = {
-         Theme = {
-           ConfigFile = "Themes/astronaut.conf";
-         };
-       };
-     };
+  services.displayManager.sddm = {
+    enable = true;
+    package = pkgs.kdePackages.sddm;
+    theme = "sddm-astronaut-theme";
+    extraPackages = with pkgs; [
+      kdePackages.qtsvg
+      kdePackages.qtmultimedia
+      kdePackages.qtvirtualkeyboard
+      kdePackages.qt5compat
+    ];
+    wayland.enable = true;
+    settings = {
+      Theme = {
+        ConfigFile = "Themes/astronaut.conf";
+      };
+    };
+  };
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
   system.stateVersion = "25.11"; # Did you read the comment?
-
 }
